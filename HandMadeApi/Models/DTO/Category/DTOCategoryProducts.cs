6dbcdf9881edc60774ac9f0053d237bb1ebcfee6
fady-shafeek <Fady.Shafeek@hotmail.com>
2022-03-27 00:17:23 +0200

    Add Dto Products

---
+++ HandMadeApi/Models/DTO/Category/DTOCategoryProducts.cs
@@ -0,0 +1,13 @@
+﻿namespace HandMadeApi.Models.DTO.Category
+{
+    public class DTOCategoryProducts
+    {
+        public string CategoryName { get; set; }
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductImage { get; set; }
+        public int Price { get; set; }
+        public int? SaleValue { get; set; }
+    }
+}
