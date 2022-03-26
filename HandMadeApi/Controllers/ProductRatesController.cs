7ca3c3f4ad51d85f54ac54c09b6a51fd127d21fb
Abanoub Harby <abanoubharby33@gmail.com>
2022-03-26 22:34:23 +0200

    DB controllers

---
+++ HandMadeApi/Controllers/ProductRatesController.cs
@@ -0,0 +1,108 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HandMadeApi.Models.StoreDatabase;
+
+namespace HandMadeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductRatesController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductRatesController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProductRates
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductRate>>> GetProductRates()
+        {
+            return await _context.ProductRates.ToListAsync();
+        }
+
+        // GET: api/ProductRates/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductRate>> GetProductRate(int id)
+        {
+            var productRate = await _context.ProductRates.FindAsync(id);
+
+            if (productRate == null)
+            {
+                return NotFound();
+            }
+
+            return productRate;
+        }
+
+        // PUT: api/ProductRates/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductRate(int id, ProductRate productRate)
+        {
+            if (id != productRate.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(productRate).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductRateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProductRates
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProductRate>> PostProductRate(ProductRate productRate)
+        {
+            _context.ProductRates.Add(productRate);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProductRate", new { id = productRate.ID }, productRate);
+        }
+
+        // DELETE: api/ProductRates/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductRate(int id)
+        {
+            var productRate = await _context.ProductRates.FindAsync(id);
+            if (productRate == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductRates.Remove(productRate);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductRateExists(int id)
+        {
+            return _context.ProductRates.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ HandMadeApi/Controllers/StoresController.cs
@@ -0,0 +1,122 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HandMadeApi.Models.StoreDatabase;
+
+namespace HandMadeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoresController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public StoresController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Stores
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Store>>> GetStores()
+        {
+            return await _context.Stores.ToListAsync();
+        }
+
+        // GET: api/Stores/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Store>> GetStore(string id)
+        {
+            var store = await _context.Stores.FindAsync(id);
+
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            return store;
+        }
+
+        // PUT: api/Stores/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStore(string id, Store store)
+        {
+            if (id != store.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(store).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StoreExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Stores
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Store>> PostStore(Store store)
+        {
+            _context.Stores.Add(store);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (StoreExists(store.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetStore", new { id = store.ID }, store);
+        }
+
+        // DELETE: api/Stores/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStore(string id)
+        {
+            var store = await _context.Stores.FindAsync(id);
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stores.Remove(store);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StoreExists(string id)
+        {
+            return _context.Stores.Any(e => e.ID == id);
+        }
+    }
+}
