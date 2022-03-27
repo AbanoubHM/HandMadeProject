6044c8464a8533c01bd4a8ecb8f23c4c5cd3f502
Abanoub Harby <abanoubharby33@gmail.com>
2022-03-27 10:48:44 +0200

    swagger

--- HandMadeApi/Program.cs
+++ HandMadeApi/Program.cs
@@ -72,10 +72,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
+//if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 app.UseCors("AllowSpecificOrigin");
 
 
