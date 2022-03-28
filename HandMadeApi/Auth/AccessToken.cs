31eff86714f966fb71fc731ad5b499afe2ef1730
Abanoub Harby <abanoubharby33@gmail.com>
2022-03-28 08:51:57 +0200

    Add roles to clients on the authorization service

---
+++ HandMadeApi/Auth/AccessToken.cs
@@ -0,0 +1,7 @@
+﻿namespace HandMadeApi.Auth {
+    public class AccessToken {
+        public string access_token { get; set; }
+        public int expires_in { get; set; }
+        public string token_type { get; set; }
+    }
+}
