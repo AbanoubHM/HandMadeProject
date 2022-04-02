cd674807a769d05e379e0c670831cb205b1b325e
fady-shafeek <Fady.Shafeek@hotmail.com>
2022-04-02 02:42:13 +0200

    Update DTOs

---
+++ HandMadeApi/Models/DTO/Cart/CartDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace HandMadeApi.Models.DTO.Cart
+{
+        public class CartDto
+        {
+            public CartHeaderDto CartHeader { get; set; }
+            public IEnumerable<CartDetailsDto> CartDetails { get; set; }
+        }
+}
---
+++ HandMadeApi/Models/DTO/Cart/CartDetailsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace HandMadeApi.Models.DTO.Cart
+{
+    public class CartDetailsDto
+    {
+        public int CartDetailsID { get; set; }
+        public int CartHeaderID { get; set; }
+        public virtual CartHeaderDto CartHeader { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
